cdcdac803c4903470d237f9dde582873c0085192
oskhen <oskar.henriksson2001@gmail.com>
2021-12-02 17:48:52 +0100

    Basic Client that sends hard-coded userdata in correct format to server

--- client.py
+++ client.py
@@ -2,6 +2,7 @@
 
 import requests
 import json
+import base64
 
 url = "http://127.0.0.1:5000/"
 
@@ -9,10 +10,42 @@ headers = {
     "Content-Type": "application/json"
 }
 
+UserEncryptedRecord = {
+    "APINAMN_Version":"0.1.0",
+    "KeyEncryptionType":"RSA2048",
+    "PayloadEncryptionType":"AES_CMC",
+    "EncryptedKey":"DataIBase64Format",
+    "EncryptedPayload":"DataIBase64Format"
+}
+
+cardUID = "C40F6C94"
+cardData = base64.b64encode(open(f"Archives/{cardUID}/LatestCard", "rb").read()).decode('utf-8')
+print(cardData)
+    
+
+DirectiveArguments = {
+    "user" : "oskhen", 
+    "cardUID" : cardUID, 
+    "data" : cardData,
+    "dev" : "False"
+}
+
+UserPayload = {
+    "IdentificationType":"PasswordHash",
+    "SendBackKey":"Base64KeyData",
+    "IdentificationData":{
+        "HashAlgorithm":"SHA256",
+        "UserName":"oskhen",
+        "PasswordHash":"Base64Hash"
+    },
+    "DirectiveName":"uploadCard",
+    "DirectiveArguments":DirectiveArguments
+}
+
 data = {
-    "user":"oskhen",
-    "data":"C40F6C94"
+    "UserEncryptedRecord" : UserEncryptedRecord,
+    "UserPayload" : UserPayload
 }
 
 r = requests.post(url, headers=headers, data=json.dumps(data))
-print(r)
+print(f"{r} - {r.text}")
